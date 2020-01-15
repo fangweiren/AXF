95d26a76a536ad4947e64e42efc1bdd5a7759bc8
fangweiren <fwr843@163.com>
2020-01-15 23:29:02 +0800

    二级数据级联查询，三级数据排序1

--- App/views.py
+++ App/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from App.models import MainWheel, MainNav, MainMustBuy, MainShop, MainShow, FoodType, Goods
+from App.views_constant import ALL_TYPE
 
 
 def home(request):
@@ -33,18 +34,29 @@ def home(request):
 
 
 def market(request):
-    return redirect(reverse('axf:market_with_params', kwargs={"typeid": 103541}))
+    return redirect(reverse('axf:market_with_params', kwargs={"typeid": 104749, "childcid": 0}))
 
 
-def market_with_params(request, typeid):
+def market_with_params(request, typeid, childcid):
     foodtypes = FoodType.objects.all()
     goods_list = Goods.objects.filter(categoryid=typeid)
+    if childcid != ALL_TYPE:
+        goods_list = goods_list.filter(childcid=childcid)
+
+    foodtype = foodtypes.get(typeid=typeid)
+    foodtypechildnames = foodtype.childtypenames
+    foodtypechildname_list = foodtypechildnames.split("#")
+    foodtype_childname_list = []
+
+    for foodtypechildname in foodtypechildname_list:
+        foodtype_childname_list.append(foodtypechildname.split(":"))
 
     data = {
         "title": "闪购",
         "foodtypes": foodtypes,
         "goods_list": goods_list,
         "typeid": int(typeid),
+        "foodtype_childname_list": foodtype_childname_list,
     }
     return render(request, 'main/market.html', context=data)
 
--- static/axf/main/js/market.js
+++ static/axf/main/js/market.js
@@ -1,7 +1,11 @@
 $(function () {
     $(".all_types").click(function () {
         $("#all_types_container").show();
-    })
+
+        var $all_type = $(this);
+        var $span = $all_type.find("span").find("span");
+        $span.removeClass("glyphicon glyphicon-chevron-down").addClass("glyphicon glyphicon-chevron-up");
+    });
 
     $(".sort_rules").click(function () {
         $("#sort_rule_container").slideDown(1000);
